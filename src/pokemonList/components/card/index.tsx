526543a11662c2a33a858d7102d0922508d71dce
Marlon Marques <marlon.belohd@gmail.com>
2024-08-17 18:24:20 -0300

    style(pokemonlist/card): add style in card and factory

--- src/pokemonList/components/card/index.tsx
+++ src/pokemonList/components/card/index.tsx
@@ -1,19 +1,44 @@
 import { Image } from "react-native";
 import { ThemedText } from "@/src/global/components/ThemedText";
 import { ThemedView } from "@/src/global/components/ThemedView";
-import { Props } from "./useCard";
+import useCard, { Props } from "./useCard";
 
 const Card = ({ name, picture }: Props) => {
   return (
     <>
       {picture && (
-        <ThemedView>
-          <Image testID="picture_id" source={{ uri: picture }} />
-          {name && <ThemedText testID="name_id">{name}</ThemedText>}
+        <ThemedView
+          style={{
+            borderColor: "white",
+            borderWidth: 2,
+            borderRadius: 16,
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 6,
+          }}
+        >
+          <Image
+            testID="picture_id"
+            source={{ uri: picture }}
+            resizeMode="contain"
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+          {name && (
+            <ThemedText testID="name_id" style={{ fontWeight: "bold" }}>
+              {name}
+            </ThemedText>
+          )}
         </ThemedView>
       )}
     </>
   );
 };
 
-export default Card;
+const CardFactory = (props: Props) => {
+  return <Card {...useCard(props)} />;
+};
+
+export default CardFactory;
