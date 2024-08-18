1dede6273c7febfe750520453fd9ede28fed1934
Marlon Marques <marlon.belohd@gmail.com>
2024-08-18 12:27:49 -0300

    test(pokemonlist): call selectPokemon function when press one pokemon of list

--- test/pokemonList/PokemonList.spec.tsx
+++ test/pokemonList/PokemonList.spec.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/src/pokemonList/components";
 import { faker } from "@faker-js/faker";
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
 import { FlatList } from "react-native";
 
 const getPokemonListFake = (length: number) => {
@@ -15,21 +15,31 @@ const getPokemonListFake = (length: number) => {
   return list;
 };
 
+const formatName = (name: string) =>
+  name.charAt(0).toUpperCase() + name.slice(1);
+
 describe("PokemonList", () => {
   test("should show pokemon list correctly", () => {
     const list = getPokemonListFake(5);
-    render(<PokemonList list={list} />);
+    render(<PokemonList list={list} selectPokemon={() => {}} />);
 
     list.forEach((pokemon, index) => {
-      expect(
-        screen.getByText(
-          pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
-        ),
-      ).toBeTruthy();
+      expect(screen.getByText(formatName(pokemon.name))).toBeTruthy();
       expect(
         screen.getAllByTestId("picture_id")[index].props.source.uri,
       ).toEqual(pokemon.picture);
     });
+  });
+
+  test("should call selectPokemon function correctly when press one pokemon of list", () => {
+    const selectPokemon = jest.fn();
+    const list = getPokemonListFake(5);
+    render(<PokemonList list={list} selectPokemon={selectPokemon} />);
+
+    fireEvent.press(screen.getAllByTestId("card_button_id")[3]);
+
+    expect(selectPokemon).toHaveBeenCalledTimes(1);
+    expect(selectPokemon).toHaveBeenCalledWith(formatName(list[3].name));
   });
 });
 
@@ -40,13 +50,14 @@ type Pokemon = {
 
 type Props = {
   list: Pokemon[];
+  selectPokemon: (name: string) => void;
 };
 
-const PokemonList = ({ list }: Props) => {
+const PokemonList = ({ list, selectPokemon }: Props) => {
   return (
     <FlatList
       data={list}
-      renderItem={({ item }) => <Card {...item} onPress={() => {}} />}
+      renderItem={({ item }) => <Card {...item} onPress={selectPokemon} />}
     />
   );
 };
